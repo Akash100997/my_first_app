3438fc3b55c16ae09767fc4b8f9bce84bd7354b4
Akash100997 <akash.alankritth@hotmail.com>
2022-09-11 13:32:48 +0530

    Use Effect Example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 
 const App = () => {
   const [Counter, setCounter] = useState(0);
+  useEffect(()=>{
+    alert("You've changed the counter to "+ Counter)
+  }, [Counter]);
   return (
     <div className="App">
       <button onClick={()=> setCounter((prevCount) => prevCount-1)}>-</button>
